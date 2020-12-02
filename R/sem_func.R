36d67ef9afdba3fe7656b38eb5c2d5352eae66e0
Ruben Lopez <rjlopez2@gmail.com>
2020-12-02 17:16:47 +0100

    adding description

--- R/sem_func.R
+++ R/sem_func.R
@@ -1,3 +1,16 @@
+#' Standard Error of the Mean (SEM)
+#'
+#' @param x A numeric vector.
+#' @param na.rm should the NA values be removed?
+#'
+#' @return The SEM from a given vector
+#' @export
+#'
+#' @examples
+#'
+#' x <- c(0:10)
+#' sem_x <- sem(x)
+#'
 sem_func <- function(x, na.rm=TRUE) { # no implemented in the main function yet
   if (na.rm) x <- na.omit(x)
   sqrt(var(x)/length(x))
