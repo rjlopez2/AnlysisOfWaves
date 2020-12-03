89c85e4c4875202082e6fc4cc6830000b38dc47a
Ruben Lopez <rjlopez2@gmail.com>
2020-12-03 16:51:56 +0100

    silence renaming of the variables to avoid errors in further names calls

--- R/clean_and_tidy_df_func.R
+++ R/clean_and_tidy_df_func.R
@@ -20,9 +20,9 @@ clean_and_tidy_df_func <- function(my_df,
            .data$Animal_No, .data$Condition, .data$n_paired, vars_for_analysis) %>%
     # names
     # filter(!is.na(Wave_deltaFF0_mean | Wave_tau_mean | Wave_speed_mean)) %>% # remove na values in the conventional way
-    filter(!is.na(syms(paste0(vars_for_analysis, collapse = " | ") ))) %>% # remove na values using symbols catch method
+    filter(!is.na(syms(paste0(vars_for_analysis, collapse = " | ") )))# %>% # remove na values using symbols catch method
     # filter(n_paired == 2) %>%
-    rename_at(vars(ends_with("_mean")), str_sub, 1, -6)
+    # rename_at(vars(ends_with("_mean")), str_sub, 1, -6)
 
   return(my_df)
 
