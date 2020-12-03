055997b4dc7c656bc0725b9cf1d8ba0f71c45df6
Ruben Lopez <rjlopez2@gmail.com>
2020-12-03 11:48:54 +0100

    adding prefix .data$ for local variables

--- R/clean_and_tidy_df_func.R
+++ R/clean_and_tidy_df_func.R
@@ -10,13 +10,14 @@
 #'
 #' @examples # The example is still missing...
 clean_and_tidy_df_func <- function(my_df,
-                                   vars_for_analysis = wave_kinet_parameters){
+                                   vars_for_analysis){
 
   my_df <- my_df %>%
-    mutate(Experiment = str_sub(string = filename, 10, 15),
-           Date_plus_experiment = str_sub(string = filename, 1, 15)) %>%
-    add_count(Date_plus_experiment, name = "n_paired") %>%
-    select(filename, Date, Experiment, Animal, Animal_No, Condition, n_paired, vars_for_analysis) %>%
+    mutate(Experiment = str_sub(string = .data$filename, 10, 15),
+           Date_plus_experiment = str_sub(string = .data$filename, 1, 15)) %>%
+    add_count(.data$Date_plus_experiment, name = "n_paired") %>%
+    select(.data$filename, .data$Date, .data$Experiment, .data$Animal,
+           .data$Animal_No, .data$Condition, .data$n_paired, vars_for_analysis) %>%
     # names
     # filter(!is.na(Wave_deltaFF0_mean | Wave_tau_mean | Wave_speed_mean)) %>% # remove na values in the conventional way
     filter(!is.na(syms(paste0(vars_for_analysis, collapse = " | ") ))) %>% # remove na values using symbols catch method
