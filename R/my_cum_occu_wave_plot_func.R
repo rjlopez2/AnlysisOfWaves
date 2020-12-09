3c8787d0c29ef4cf9f6c058262c559ac77698bf5
Ruben Lopez <rjlopez2@gmail.com>
2020-12-09 22:09:12 +0100

    adding pakg name prefix to functions andlittle osmetic details

--- R/my_cum_occu_wave_plot_func.R
+++ R/my_cum_occu_wave_plot_func.R
@@ -4,7 +4,6 @@
 #'
 #' @param my_dataset A dataframe with raw data of waves.
 #' @param reffer_wave_thres A double. Reference value i n seconds indicating the threshold for the calculus of cumulative waves occurrence.
-#' @param font_size
 #'
 #' @return
 #' @export
@@ -13,28 +12,28 @@
 my_cum_occu_wave_plot_func <- function(my_dataset, reffer_wave_thres){
 
   my_cum_occu_plot <- my_dataset %>%
-    ggplot(aes(x = Wave_latency)) +
-    geom_rect(aes(ymin = -Inf,
-                  ymax = Inf,
-                  xmin = 0,
-                  xmax = reffer_wave_thres),
-              alpha = 0.5, fill = "#f0f0f0",) +
+    ggplot2::ggplot(ggplot2::aes(x = .data$Wave_latency)) +
+    ggplot2::geom_rect(ggplot2::aes(ymin = -Inf,
+                                    ymax = Inf,
+                                    xmin = 0,
+                                    xmax = reffer_wave_thres),
+                       alpha = 0.5, fill = "#f0f0f0",) +
     # facet_grid(~Condition) +
     # geom_histogram(aes(fill = Condition))
     # stat_ecdf(aes(color = interaction(Animal, Condition)), na.rm = T, show.legend = F) #+
-    stat_ecdf(aes(color = Animal,
-                  group = interaction(Animal, Condition),
-                  linetype = Condition),
-              na.rm = T,
-              show.legend = T,
-              pad = T,
-              lwd = 1) +
-    # pptx_presentation_theme_func(font_size) +
-    scale_colour_manual(values = c("#666666", "#CC0000")) +
-    scale_y_continuous(labels = function(x){x*100}) +
-    labs(x = "Time (s)", y = "Cumulative wave\noccurrence (%)") +
-    theme(legend.position = c(1, 0.4),
-          legend.key=element_blank())
+    ggplot2::stat_ecdf(ggplot2::aes(color = .data$Animal,
+                                    group = interaction(.data$Animal, .data$Condition),
+                                    linetype = .data$Condition),
+                       na.rm = T,
+                       show.legend = T,
+                       pad = T,
+                       lwd = 1) +
+    pptx_presentation_theme_func() +
+    ggplot2:: scale_colour_manual(values = c("#666666", "#CC0000")) +
+    ggplot2::scale_y_continuous(labels = function(x){x*100}) +
+    ggplot2::labs(x = "Time (s)", y = "Cumulative wave\noccurrence (%)") +
+    ggplot2::theme(legend.position = c(1, 0.4),
+                   legend.key=ggplot2::element_blank())
   # theme(legend.key=element_blank())
 
   return(list(cum_occu_plot = my_cum_occu_plot))
