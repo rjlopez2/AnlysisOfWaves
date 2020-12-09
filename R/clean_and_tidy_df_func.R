97cd24f1d901a2a4b10adc7f2804cee6f0a91ebc
Ruben Lopez <rjlopez2@gmail.com>
2020-12-09 21:38:25 +0100

    fix "n_paired" computation in function "add_expe_and_counter_func"

--- R/clean_and_tidy_df_func.R
+++ R/clean_and_tidy_df_func.R
@@ -2,10 +2,10 @@
 #'
 #' Clean and organize your dataset
 #'
-#' @param my_df A dataframe from the institute
+#' @param my_df A dataframe with waves raw data. Must contain "Date" and "Experiment" columns.
 #' @param vars_for_analysis A character vector with variable names to be analyzed.
 #'
-#' @return A clean dataframe ready for statistics analysis or plotting
+#' @return A clean dataframe ready for statistics analysis or plotting and add column "n_paired" for paired experiments.
 #' @export
 #'
 #' @examples # The example is still missing...
@@ -13,12 +13,9 @@ clean_and_tidy_df_func <- function(my_df,
                                    vars_for_analysis){
 
   my_df <- my_df %>%
-    mutate(Experiment = str_sub(string = .data$filename, 10, 15),
-           Date_plus_experiment = str_sub(string = .data$filename, 1, 15)) %>%
-    add_count(.data$Date_plus_experiment, name = "n_paired") %>%
+    add_count(.data$Date, .data$Experiment, name = "n_paired") %>%
     select(.data$filename, .data$Date, .data$Experiment, .data$Animal,
            .data$Animal_No, .data$Condition, .data$n_paired, vars_for_analysis) %>%
-    # names
     # filter(!is.na(Wave_deltaFF0_mean | Wave_tau_mean | Wave_speed_mean)) %>% # remove na values in the conventional way
     filter(!is.na(syms(paste0(vars_for_analysis, collapse = " | ") )))# %>% # remove na values using symbols catch method
     # filter(n_paired == 2) %>%
