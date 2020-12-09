456980e7aca5c5c44187b66a7f7c7d5c9643f6f4
Ruben Lopez <rjlopez2@gmail.com>
2020-12-09 20:56:35 +0100

    adding description to function "add_expe_and_counter_func"

--- R/add_expe_and_counter_func.R
+++ R/add_expe_and_counter_func.R
@@ -1,19 +1,19 @@
 
 #' add_expe_and_counter_func
 #'
-#' Add couter for each experiment.
+#' Add counter for each experiment to asses paired experiment.
 #'
-#' @param my_dataset
+#' @param my_dataset A dataframe containing variables filename.
 #'
-#' @return
+#' @return the dataframe and a new column named "n_paired" with values 1 for non-paired experiment or 2 pro paired experiments.
 #' @export
 #'
-#' @examples
+#' @examples # The example is still missing...
 add_expe_and_counter_func <- function(my_dataset){
   my_dataset <-  my_dataset %>%
-    mutate(Experiment = str_sub(string = filename, 10, 15),
-           Date_plus_experiment = str_sub(string = filename, 1, 15)) %>%
-    add_count(Date_plus_experiment, name = "n_paired")
+    mutate(Experiment = str_sub(string = .data$filename, 10, 15),
+           Date_plus_experiment = str_sub(string = .data$filename, 1, 15)) %>%
+    add_count(.data$Date_plus_experiment, name = "n_paired")
 
   return(my_dataset)
 }
