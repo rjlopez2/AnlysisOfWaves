804f18570650386d11ba41a7bfd591a5b3a656f1
Ruben Lopez <rjlopez2@gmail.com>
2020-12-09 22:32:48 +0100

    adding prefix pkg name for functions call

--- R/my_occu_bar_plot_func.R
+++ R/my_occu_bar_plot_func.R
@@ -12,30 +12,30 @@
 my_occu_bar_plot_func <- function(my_dataset, reffer_wave_thres){
 
   my_bar_plot <- my_dataset %>%
-    mutate(new_waves = if_else(df40_o$Wave_latency <= reffer_wave_thres &
-                                 !is.na(df40_o$Wave_latency) &
-                                 df40_o$Wave_latency > 0, T, F)) %>%
-    group_by(Animal, Condition, new_waves) %>%
-    select(Animal, Condition, new_waves) %>%
+    mutate(new_waves = if_else(.data$Wave_latency <= reffer_wave_thres &
+                                 !is.na(.data$Wave_latency) &
+                                 .data$Wave_latency > 0, T, F)) %>%
+    group_by(.data$Animal, .data$Condition, .data$new_waves) %>%
+    select(.data$Animal, .data$Condition, .data$new_waves) %>%
     summarise(count = n()) %>%
     # View
     mutate(Percentage = count/sum(count) * 100) %>%
-    filter(new_waves == T) %>%
+    filter(.data$new_waves == T) %>%
     # View
-    ggplot(aes(x = Animal,
-               y = Percentage,
-               fill = new_waves,
-               alpha = 0.5))  +
-    geom_bar(stat="identity",
-             color = "black",
-             aes(fill = Animal),
-             size = 1) +
-    scale_y_continuous(limits = c(0, 100)) +
-    facet_grid(. ~ Condition) +
+    ggplot2::ggplot(ggplot2::aes(x = .data$Animal,
+                                 y = .data$Percentage,
+                                 fill = .data$new_waves,
+                                 alpha = 0.5))  +
+    ggplot2::geom_bar(stat="identity",
+                      color = "black",
+                      ggplot2::aes(fill = .data$Animal),
+                      size = 1) +
+    ggplot2::scale_y_continuous(limits = c(0, 100)) +
+    ggplot2::facet_grid(. ~ .data$Condition) +
     pptx_presentation_theme_func() +
-    scale_fill_manual(values = c("#666666", "#CC0000")) +
-    labs(x = "Animal", y = "Wave occurrence (%)") + # Add legend
-    theme(legend.position = "none")
+    ggplot2::scale_fill_manual(values = c("#666666", "#CC0000")) +
+    ggplot2::labs(x = "Animal", y = "Wave occurrence (%)") + # Add legend
+    ggplot2::theme(legend.position = "none")
 
   # my_bar_plot
 
