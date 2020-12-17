79a08959742654a600951808aa71829b03bcce4c
Ruben Lopez <rjlopez2@gmail.com>
2020-12-17 22:46:55 +0100

    creating file for "make_profile_func" function

---
+++ R/make_profile_func.R
@@ -0,0 +1,20 @@
+#' make_profile_func
+#'
+#' Create plot-profile from linescan image
+#'
+#' @param my_dir "A directory with a linescan image in tiff format.
+#'
+#' @return two vector: "Time" in ms and "Fluorescence" the signal in AU.
+#' @export
+#'
+#' @examples # example missing.
+#'
+make_profile_func <- function(my_dir){
+  my_trace <- my_dir %>%
+    ijtiff::read_tif() %>%
+    `[` ( , , 1, 1) %>%
+    rowMeans() %>%
+    dplyr::tibble(Time = 1:length(.data) * 2 / 1000, # set the time in seconds
+           Fluorescence = .data)
+}
+
