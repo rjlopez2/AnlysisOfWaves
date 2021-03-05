07ce92701ae18f9ca4a752c40449b63a498e7c4b
Ruben Lopez <rjlopez2@gmail.com>
2021-03-05 14:43:02 +0100

    vars_for_analysis fixed to work with tidyevaluation on multiples values (strings names of var to analyze)

--- R/clean_and_tidy_df_func.R
+++ R/clean_and_tidy_df_func.R
@@ -9,15 +9,14 @@
 #' @export
 #'
 #' @examples # The example is still missing...
-clean_and_tidy_df_func <- function(my_df,
-                                   vars_for_analysis){
+clean_and_tidy_df_func <- function(my_df, vars_for_analysis){
 
   my_df <- my_df %>%
     add_count(.data$Date, .data$Experiment, name = "n_paired") %>%
     select(.data$filename, .data$Date, .data$Experiment, .data$Animal,
-           .data$Animal_No, .data$Condition, .data$n_paired, vars_for_analysis) %>%
+           .data$Animal_No, .data$Condition, .data$n_paired, {{vars_for_analysis}}) %>%
     # filter(!is.na(Wave_deltaFF0_mean | Wave_tau_mean | Wave_speed_mean)) %>% # remove na values in the conventional way
-    filter(!is.na(syms(paste0(vars_for_analysis, collapse = " | ") )))# %>% # remove na values using symbols catch method
+    filter(!is.na(syms(paste0({{vars_for_analysis}}, collapse = " | ") )))# %>% # remove na values using symbols catch method
     # filter(n_paired == 2) %>%
     # rename_at(vars(ends_with("_mean")), str_sub, 1, -6)
 
