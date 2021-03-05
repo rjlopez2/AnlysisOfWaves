7949616ff6ec076655343704fe3c4748838ae47b
Ruben Lopez <rjlopez2@gmail.com>
2021-03-05 15:21:45 +0100

    "vars_prefix_to_summ". add a new argument to the function that facilitate to specify which group of variables to summarize.must enter the prefix for Cyto or SS

--- R/my_summ_stat_func.R
+++ R/my_summ_stat_func.R
@@ -3,17 +3,26 @@
 #' Compute the summary statistics of the multiples variables from the wave analysis from a previosly cleaned dataset
 #'
 #' @param my_dataset A dataframe prevously cleaned with the fucntion "lalala".
+#' @param vars_prefix_to_summ a string indicating which variables you want to summarize. Only one of twon values possible: "Cyto" or "SR".
 #'
 #' @return compute the mean, median, SD, SEM, number of waves, number of cells, number of animals, and asses Normality of the distribution with the Shapiro-Wilk test.
 #' @export
 #'
 #' @examples # The example is still missing...
-my_summ_stat_func <- function(my_dataset){
+my_summ_stat_func <- function(my_dataset, vars_prefix_to_summ = c("Cyto", "SR")){
+
+  if (vars_prefix_to_summ == "Cyto"){
+    vars_prefix_to_summ <- "SR"
+  }else if(vars_prefix_to_summ == "SR"){
+    vars_prefix_to_summ <- "Cyto"
+  }else(stop("You must select 'Cyto' or 'SR' for the variables to summarize"))
+
+
 
   my_funs_names <- c("mean", "sd", "sem", "median", "n_Waves", "Normality_Shapiro_p")
 
   stats_by_cells <- my_dataset %>% group_by(.data$Animal, .data$Condition) %>%
-    select(!(starts_with("SR")|starts_with("n_pa"))) %>%
+    select(!(starts_with(vars_prefix_to_summ)|starts_with("n_pa"))) %>%
 
     summarise_if(is.numeric, .funs = funs(n_Waves = length,
                                           mean = mean,
