68450e21f5897e4c0663a51dfe265218c4791e3d
Ruben Lopez <rjlopez2@gmail.com>
2021-03-05 15:41:56 +0100

    argument "group_1" now accept multiples strings. update also the documentation

--- R/wilcxntest_by_group_multivar_func.R
+++ R/wilcxntest_by_group_multivar_func.R
@@ -4,8 +4,8 @@
 #'
 #' @param my_dataset A cleaned dataframe.
 #' @param my_var_set A character vector with the names of variables to test.
-#' @param group_1 Character. the name for the first grouping comparison variable.
-#' @param group_2 Character. the name for the second grouping comparison variable.
+#' @param group_1 Character of one or more values. The name for the first grouping comparison variable. Default to `"Condition"`.
+#' @param group_2 Character. the name for the second grouping comparison variable. Default to `"Animal"`.
 #'
 #' @return a datatable with the output of the Wilcoxon test
 #' @export
@@ -17,16 +17,16 @@ wilcxntest_by_group_multivar_func <- function(my_dataset, my_var_set,
 
   result_table <- purrr::map_dfr(my_var_set, function(my_var){
 
-    group_1 <- sym(group_1)
+    group_1 <- syms(group_1)# add multiples strings with sym(s)
     group_2 <- sym(group_2)
     my_var <- sym(my_var)
 
     my_dataset %>%
-      group_by(!!group_1) %>%
+      group_by(!!!group_1) %>%
       rstatix::pairwise_wilcox_test(formula = stats::formula(expr(!!my_var ~ !!group_2)),
-                           p.adjust.method = "BH",
-                           detailed = TRUE,
-                           paired = FALSE) # this was f**ing tricky to make it work -> check tidy evaluation and fucntions: "expr", "formula", "eval".
+                                    p.adjust.method = "BH",
+                                    detailed = TRUE,
+                                    paired = FALSE) # this was f**ing tricky to make it work -> check tidy evaluation and fucntions: "expr", "formula", "eval".
   })
 
   result_table
