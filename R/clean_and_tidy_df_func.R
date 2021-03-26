0fb64ece7f1e034cce02a8f0ab8300bc9ff646ff
Ruben Lopez <rjlopez2@gmail.com>
2021-03-26 23:05:16 +0100

    add helper wrapper function for handeling "Treatment" column missing in the intact cells dataset

--- R/clean_and_tidy_df_func.R
+++ R/clean_and_tidy_df_func.R
@@ -9,18 +9,26 @@
 #' @export
 #'
 #' @examples # The example is still missing...
+#'
 clean_and_tidy_df_func <- function(my_df, vars_for_analysis){
 
   my_df <- my_df %>%
-    add_count(.data$Date, .data$Experiment, name = "n_paired") %>%
-    select(.data$filename, .data$Date, .data$Experiment, .data$Animal,
-           .data$Animal_No, .data$Condition,  .data$Treatment, .data$n_paired,
+    add_count(.data$Date,
+              .data$Experiment,
+              name = "n_paired") %>%
+    select(.data$filename,
+           .data$Date,
+           .data$Experiment,
+           .data$Animal,
+           .data$Animal_No,
+           .data$Condition,
+           any_of(.data$Treatment), # use this helper for the intact cells dataset which doesn't contain this column
+           .data$n_paired,
            all_of({{vars_for_analysis}})) %>%
     # filter(!is.na(Wave_deltaFF0_mean | Wave_tau_mean | Wave_speed_mean)) %>% # remove na values in the conventional way
     filter(!is.na(syms(paste0({{vars_for_analysis}}, collapse = " | ") )))# %>% # remove na values using symbols catch method
-    # filter(n_paired == 2) %>%
-    # rename_at(vars(ends_with("_mean")), str_sub, 1, -6)
+  # filter(n_paired == 2) %>%
+  # rename_at(vars(ends_with("_mean")), str_sub, 1, -6)
 
   return(my_df)
-
 }
