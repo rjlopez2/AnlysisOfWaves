1def21ff507cef7f5921140f16a78ed5b003b21f
Ruben Lopez <rjlopez2@gmail.com>
2021-05-27 22:57:38 +0200

    fix issue handeling the animal factors for ploting. Update  documentation.

--- R/superplot_func.R
+++ R/superplot_func.R
@@ -7,6 +7,7 @@
 #' @param yaxe String. name of the variable in the y axis.
 #' @param faceted_by_1 String. Define facet for plotting.
 #' @param faceted_by_2 String. Define facet for plotting.
+#' @param grouping String vector of lenght >=1. Names of the grouping variables to compute summarize stats.
 #' @param jitter_width Double. Define how big is the with of the scatter points.
 #' @param cell_size Double. define the size of the cell level scatter plot.
 #' @param cell_alpha Double. define the transparency of the cell level scatter plot.
@@ -19,22 +20,34 @@
 #'
 #' @examples # The example is still missing...
 
-superplot_func <- function(dataset, xaxe, yaxe,
-                           faceted_by_1 = "Condition",
-                           faceted_by_2 = ".",
-                           jitter_width = 3.5,
-                           cell_size = 2,
-                           cell_alpha = 0.4,
-                           animal_size = 4,
-                           animal_alpha = 0.8,
-                           ...){
+superplot_func_test <- function(dataset, xaxe, yaxe,
+                                faceted_by_1 = "Condition",
+                                faceted_by_2 = ".",
+                                grouping = c("Animal",
+                                             "Treatment",
+                                             "Animal_No",
+                                             "Condition"),
+                                jitter_width = 3.5,
+                                cell_size = 2,
+                                cell_alpha = 0.4,
+                                animal_size = 4,
+                                animal_alpha = 0.8,
+                                ...){
+  WT_fct <- dataset$Animal %>%
+    levels() %>%
+    dplyr::nth(1)
+  CPVT_fct <- dataset$Animal %>%
+    levels() %>%
+    dplyr::nth(2)
+
   xaxe <- sym(xaxe)
   yaxe <- sym(yaxe)
   yaxe_mean <- sym(paste("mean", yaxe, sep = "_"))
 
   ## Create the summarized dataset (aggregated by the mean)
   summ_dataset <- dataset %>%
-    group_by(.data$Animal, .data$Animal_No, .data$Condition ) %>%
+    group_by(across(any_of(grouping))) %>%
+    # group_by(.data$Animal, .data$Animal_No, .data$Condition, any_of(.data$Treatment) ) %>%
     summarise(across(where(is.numeric), list(mean = ~ mean(.x, na.rm = TRUE)),
                      .names = "{.fn}_{.col}"), .groups = 'drop')
 
@@ -42,7 +55,7 @@ superplot_func <- function(dataset, xaxe, yaxe,
   base_plot <-
     ggplot2::ggplot(data = dataset,
                     ggplot2::aes(x = {{xaxe}},
-               y = {{yaxe}})) +
+                                 y = {{yaxe}})) +
     ggplot2::geom_violin(ggplot2::aes(color = {{xaxe}})) +
     ggplot2::facet_grid(stats::reformulate(faceted_by_1, faceted_by_2)) +
     ggplot2::scale_colour_manual(values = c("#666666", "#CC0000"))
@@ -53,58 +66,58 @@ superplot_func <- function(dataset, xaxe, yaxe,
     ## WT layer
     ggnewscale::new_scale("fill") +
     ggbeeswarm::geom_beeswarm(data = {dataset %>%
-        filter(.data$Animal == "WT")},
+        filter(.data$Animal == WT_fct)},
         ggplot2::aes(y = {{yaxe}},
-            fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
+                     fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
         priority = "density",
         cex= jitter_width,
         size = cell_size,
         shape = 21,
         alpha = cell_alpha) +
     colorspace::scale_fill_discrete_sequential(palette = "Blues",
-                                   l2 = 50,
-                                   c2 = 10,
-                                   h2 = 200) +
-    ## WT layer
+                                               l2 = 50,
+                                               c2 = 10,
+                                               h2 = 200) +
+    ## CPVT layer
     ggnewscale::new_scale("fill") +
     ggbeeswarm::geom_beeswarm(data = {dataset %>%
-        filter(.data$Animal == "CPVT")},
+        filter(.data$Animal == CPVT_fct)},
         ggplot2::aes(y = {{yaxe}},
-            fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
+                     fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
         priority = "density",
         cex= jitter_width,
         size = cell_size,
         shape = 21,
         alpha = cell_alpha) +
     colorspace::scale_fill_discrete_sequential(palette ="Reds",
-                                   l1 = 10, l2 = 80,
-                                   c1 = 150, c2 = 200) +
+                                               l1 = 10, l2 = 80,
+                                               c1 = 150, c2 = 200) +
     ## Add Animal level data (BIG scatterplot)
     ## WT layer
     ggnewscale::new_scale("fill") +
     ggbeeswarm::geom_beeswarm(data = {summ_dataset %>%
-        filter(.data$Animal == "WT")},
+        filter(.data$Animal == WT_fct)},
         ggplot2::aes(y = {{yaxe_mean}},
-            fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
+                     fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
         size = animal_size,
         shape = 21,
         alpha = animal_alpha) +
     colorspace::scale_fill_discrete_sequential(palette = "Blues",
-                                   l2 = 50,
-                                   c2 = 10,
-                                   h2 = 200) +
+                                               l2 = 50,
+                                               c2 = 10,
+                                               h2 = 200) +
     ## CPVT layer
     ggnewscale::new_scale("fill") +
     ggbeeswarm::geom_beeswarm(data = {summ_dataset %>%
-        filter(.data$Animal == "CPVT")},
+        filter(.data$Animal == CPVT_fct)},
         ggplot2::aes(y = {{yaxe_mean}},
-            fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
+                     fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
         size = animal_size,
         shape = 21,
         alpha = animal_alpha) +
     colorspace::scale_fill_discrete_sequential(palette ="Reds",
-                                   l1 = 10, l2 = 80,
-                                   c1 = 150, c2 = 200) +
+                                               l1 = 10, l2 = 80,
+                                               c1 = 150, c2 = 200) +
     # remove legend
     ggplot2::theme(legend.position="none") +
     pptx_presentation_theme_func()
