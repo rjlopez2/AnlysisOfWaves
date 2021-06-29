b96f1c1ecd2c5a278d386aa8119a2dae4657ea3b
Ruben Lopez <rjlopez2@gmail.com>
2021-06-29 21:36:04 +0200

    change base font and size factor to a more reasonable size for the theme plot function

--- R/pptx_presentation_theme_func.R
+++ R/pptx_presentation_theme_func.R
@@ -9,12 +9,12 @@
 #' @details This take a ggplot2 object, usually a scatter plot and change the theme to almost totally clear.
 #' @examples # The example is still missing...
 #'
-pptx_presentation_theme_func <- function(base_size = 15){
+pptx_presentation_theme_func <- function(base_size = 18){
   structure(list(
-    axis.title = ggplot2::element_text(size = base_size * 1.2 ,
-                              vjust = 1),
-    axis.text = ggplot2::element_text(size = base_size * 0.8 ,
-                             vjust = 1,
+    axis.title = ggplot2::element_text(size = base_size * 1.2 ),
+                              # vjust = 1),
+    axis.text = ggplot2::element_text(size = base_size * 0.9 ,
+                             # vjust = 1,
                              colour = "black"),
     # panel.border = ggplot2::element_blank(),
     # panel.background = ggplot2::element_blank(),
