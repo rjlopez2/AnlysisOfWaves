2c62737c31a09ac1b399e511bce132a11e6909a8
Ruben Lopez <rjlopez2@gmail.com>
2021-07-06 14:10:06 +0200

    rewrite the superplot_func function so it's adapted to the new plot layer functions definition

--- R/superplot_func.R
+++ R/superplot_func.R
@@ -1,130 +1,91 @@
 #' Superplot with violin and scatter plot
 #'
-#' Make a violin and scatter plot with aggregated data at different levels (animal, cell, etc).
+#' Make a violin and scatter plot with aggregated data at different levels (animal, cell or waves). Note: this function is depending on other functions to build the multiples layers up.
 #'
-#'@param dataset A dataframe with raw data of waves.
-#' @param xaxe String. name of the variable in the x axis.
-#' @param yaxe String. name of the variable in the y axis.
-#' @param faceted_by_1 String. Define facet for plotting.
-#' @param faceted_by_2 String. Define facet for plotting.
-#' @param grouping String vector of lenght >=1. Names of the grouping variables to compute summarize stats.
-#' @param jitter_width Double. Define how big is the with of the scatter points.
-#' @param cell_size Double. define the size of the cell level scatter plot.
-#' @param cell_alpha Double. define the transparency of the cell level scatter plot.
-#' @param animal_size Double. define the size of the animal level scatter plot.
-#' @param animal_alpha Double. define the transparency of the animal level scatter plot.
-#' @param ... Other parameters to pass to the function.
+#' @param dataset A dataframe object. The dataset for analysis, eg wave kinetics or wave occurrence df.
+#' @param yaxe A string. The variable name of interest to plot.
+#' @param base_violin A string. Which base plot would you like to plot as a violin plot?. Options are: 1. `waves`, 2. `cells`. Default to `cells`.
+#' @param plot_type A string. Which type of data would you like to see in the next layer?. Options are: 1. `waves`, 2. `cells`, 3. `none`. Default to `cells`.
+#' @param animal_layer A logic value. Would you like to add the Animal dot layer? default to `TRUE`.
+#' @param wave_size A double. Value assigned to the size of the dots. Default to `2`.
+#' @param wave_alpha A double. Value assigned to the transparency of the dots. Default to `0.4`.
+#' @param cell_size A double. Value assigned to the size of the dots. Default to `2.5`.
+#' @param cell_alpha A double. Value assigned to the transparency of the dots. Default to `0.4`.
+#' @param animal_size A double. Value assigned to the size of the dots. Default to `5`.
+#' @param animal_alpha A double. Value assigned to the transparency of the dots. Default to `0.6`.
+#' @param base_font_size A integer. Modify the size of the fonts. Default to 15.
 #'
-#' @return A ggplot object with boxplot + jitter scatter plot.
+#' @return A ggplot object with a superplot indicating multiples layers of  Animals, cell or waves.
 #' @export
 #'
-#' @examples # The example is still missing...
-
-superplot_func <- function(dataset, xaxe, yaxe,
-                                faceted_by_1 = "Condition",
-                                faceted_by_2 = ".",
-                                grouping = c("Animal",
-                                             "Treatment",
-                                             "Animal_No",
-                                             "Condition"),
-                                jitter_width = 3.5,
-                                cell_size = 2,
+#' @examples
+superplot_func_test <- function(dataset,
+                                yaxe,
+                                base_violin = "cells",
+                                plot_type = "cells",
+                                animal_layer = TRUE,
+                                wave_size = 2,
+                                wave_alpha = 0.4,
+                                cell_size = 2.5,
                                 cell_alpha = 0.4,
-                                animal_size = 4,
-                                animal_alpha = 0.8,
-                                ...){
-  WT_fct <- dataset$Animal %>%
-    levels() %>%
-    dplyr::nth(1)
-  CPVT_fct <- dataset$Animal %>%
-    levels() %>%
-    dplyr::nth(2)
+                                animal_size = 5,
+                                animal_alpha = 0.6,
+                                base_font_size = 15){
 
-  xaxe <- sym(xaxe)
-  yaxe <- sym(yaxe)
-  yaxe_mean <- sym(paste("mean", yaxe, sep = "_"))
+  ####################################################################################
+  ### this function the different plotting layers for the final superplot
+  ### may be need some twitch for better flexibility
+  ####################################################################################
 
-  ## Create the summarized dataset (aggregated by the mean)
-  summ_dataset <- dataset %>%
-    group_by(across(any_of(grouping))) %>%
-    # group_by(.data$Animal, .data$Animal_No, .data$Condition, any_of(.data$Treatment) ) %>%
-    summarise(across(where(is.numeric), list(mean = ~ mean(.x, na.rm = TRUE)),
-                     .names = "{.fn}_{.col}"), .groups = 'drop')
+  # create base violin plot canvas based on waves or cells, default to cells
+  switch (base_violin,
 
-  ## Create base layer plot
-  base_plot <-
-    ggplot2::ggplot(data = dataset,
-                    ggplot2::aes(x = {{xaxe}},
-                                 y = {{yaxe}})) +
-    ggplot2::geom_violin(ggplot2::aes(color = {{xaxe}}),
-                         size = 1) +
-    ggplot2::facet_grid(stats::reformulate(faceted_by_1, faceted_by_2)) +
-    ggplot2::scale_colour_manual(values = c("#666666", "#CC0000"))
+          waves = superplot <- dataset %>%
+            cell_layer_vio_ggplot_func(yaxe = yaxe),
 
+          cells = superplot <- dataset %>%
+            cell_layer_vio_ggplot_func(yaxe = yaxe),
 
-  base_plot <- base_plot +
-    ## Add Cell level data (SMALL scatterplot)
-    ## WT layer
-    ggnewscale::new_scale("fill") +
-    ggbeeswarm::geom_beeswarm(data = {dataset %>%
-        filter(.data$Animal == WT_fct)},
-        ggplot2::aes(y = {{yaxe}},
-                     fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
-        priority = "density",
-        cex= jitter_width,
-        size = cell_size,
-        shape = 21,
-        alpha = cell_alpha) +
-    colorspace::scale_fill_discrete_sequential(palette = "Blues",
-                                               l2 = 50,
-                                               c2 = 10,
-                                               h2 = 200) +
-    ## CPVT layer
-    ggnewscale::new_scale("fill") +
-    ggbeeswarm::geom_beeswarm(data = {dataset %>%
-        filter(.data$Animal == CPVT_fct)},
-        ggplot2::aes(y = {{yaxe}},
-                     fill = interaction({{xaxe}}, .data$Animal_No, sep = "_")),
-        priority = "density",
-        cex= jitter_width,
-        size = cell_size,
-        shape = 21,
-        alpha = cell_alpha) +
-    colorspace::scale_fill_discrete_sequential(palette ="Reds",
-                                               l1 = 10, l2 = 80,
-                                               c1 = 150, c2 = 200) +
-    ## Add Animal level data (BIG scatterplot)
-    ## WT layer
-    ggnewscale::new_scale("fill") +
-    ggbeeswarm::geom_beeswarm(data = {summ_dataset %>%
-        filter(.data$Animal == WT_fct)},
-        ggplot2::aes(y = {{yaxe_mean}},
-                     fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
-        size = animal_size,
-        shape = 21,
-        alpha = animal_alpha) +
-    colorspace::scale_fill_discrete_sequential(palette = "Blues",
-                                               l2 = 50,
-                                               c2 = 10,
-                                               h2 = 200) +
-    ## CPVT layer
-    ggnewscale::new_scale("fill") +
-    ggbeeswarm::geom_beeswarm(data = {summ_dataset %>%
-        filter(.data$Animal == CPVT_fct)},
-        ggplot2::aes(y = {{yaxe_mean}},
-                     fill = interaction(.data$Animal, .data$Animal_No, sep = "_")),
-        size = animal_size,
-        shape = 21,
-        alpha = animal_alpha) +
-    colorspace::scale_fill_discrete_sequential(palette ="Reds",
-                                               l1 = 10, l2 = 80,
-                                               c1 = 150, c2 = 200) +
-    # remove legend
-    ggplot2::theme(legend.position="none") +
-    pptx_presentation_theme_func(...)
+          stop("Invalid `base_violin` value. You must select a violin basic plot. Options are: 1. `waves`, 2. `cells`")
+
+  )
+
+  # create cell or wave dot plot layer, default to cells
+  switch (plot_type,
+          cells = superplot <- superplot %>%
+            cell_layer_point_ggplot_func(dataset = dataset,
+                                         yaxe = yaxe,
+                                         jitter_width = 3,
+                                         cell_alpha = cell_alpha,
+                                         cell_size = cell_size),
+          waves = superplot <- superplot %>%
+            wave_layer_point_ggplot_func(dataset = dataset,
+                                         yaxe = yaxe,
+                                         jitter_width = 3,
+                                         wave_size = wave_size,
+                                         wave_alpha = wave_alpha),
+          none = superplot,
+
+          stop("Invalid `plot_type` value. You must select a dot (small) plot type. Options are: 1. `waves`, 2. `cells`, 3. `none`")
+
+  )
 
 
-  rm(summ_dataset)
-  return(base_plot)
+  ##### ad last layer with animal dots (big)
+  if(animal_layer){
+    superplot <- superplot %>%
+      animal_layer_point_ggplot_func(dataset = dataset,
+                                     yaxe = yaxe,
+                                     animal_size = animal_size,
+                                     animal_alpha = animal_alpha,
+                                     jitter_width = 0)
+  }
+
+  superplot <- superplot +
+    ggplot2::theme(legend.position = "none") +
+    pptx_presentation_theme_func(base_font_size)
+
+
+  return(superplot)
 
 }
