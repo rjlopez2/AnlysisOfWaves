37e550ced74e44316115567e5b2b1d34e885ddf2
Ruben Lopez <rjlopez2@gmail.com>
2021-07-16 20:34:51 +0200

    Update description and fix typos in the function

--- R/normalize_sr_data_func.R
+++ R/normalize_sr_data_func.R
@@ -1,6 +1,6 @@
 #' Normalize to control your values
 #'
-#' This function take a previously aggregated dataset using the function `summarize_sr_by_cell_func()` end normalize all numeric variables to the control conditions. hte new dataset contain a variable call `Var_type` witch contain the `Normalized` value or the `Raw` value.
+#' This function take a previously aggregated by cell dataset using the function `summarize_sr_by_cell_func()` end normalize all numeric variables to the control conditions. The new dataset contain a new variable called `Var_type` witch contain the `"Normalized"` value or the `"Raw"` value.
 #' @param aggre_by_cell_dataset A Dataframe. A previously aggregated by cell dataset.
 #'
 #' @return A dataframe with nomrlaize numeric variables to control.
@@ -9,15 +9,15 @@
 #' @examples # the example is missing
 normalize_sr_data_func <- function(aggre_by_cell_dataset){
 
-  filtered_dataset <- filtered_dataset %>%
+  aggre_by_cell_dataset <- aggre_by_cell_dataset %>%
     pivot_longer(cols = where(is.double),
                  names_to = "Parameters") %>%
     pivot_wider(names_from = .data$Condition,
                 values_from = where(is.double)) %>%
-    mutate(across(where(is.double),
+    mutate(across(where(is.double) ,
                   list("Normalized" = ~. / Control))) %>%
-    mutate(across(.data$Control_Normalized,
-                  ~ .data$Control / mean(.data$Control))) %>%
+    group_by(.data$Treatment, .data$Animal) %>%
+    mutate(Control_Normalized =  .data$Control / mean(.data$Control, na.rm = T)) %>%
     pivot_longer(cols = where(is.double),
                  names_to = "Condition") %>%
     mutate(Var_type = if_else(str_detect(.data$Condition, "_Normalized$"),
@@ -25,9 +25,8 @@ normalize_sr_data_func <- function(aggre_by_cell_dataset){
            Condition = forcats::as_factor(str_remove(.data$Condition, "_Normalized"))) %>%
     filter_all(all_vars(!is.na(.))) %>%
     pivot_wider(names_from = .data$Parameters,
-                values_from = .data$value) %>%
-    ungroup
+                values_from = .data$value)
 
-  return(filtered_dataset)
+  return(aggre_by_cell_dataset)
 
 }
