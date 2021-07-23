c053455dda31c7b1417c5b30e83248a500d128c3
Ruben Lopez <rjlopez2@gmail.com>
2021-07-23 11:03:37 +0200

    deleting old things in playground file

--- data-raw/playground_1.R
+++ data-raw/playground_1.R
@@ -4,8 +4,13 @@ load_all()
 document()
 check()
 install()
+
+# load("~/Dropbox/r_stat/code/final_script/clean_SR_data.RData")
+# clean_SR_data <- filtered_df
+# use_data(clean_SR_data, overwrite = TRUE)
 # use_data_raw()
-file.create("data-raw/playground_2.R")
+# file.create("data-raw/playground_2.R")
+use_r("clean_SR_data")
 
 use_r("wilcxntest_by_group_multivar_func")
 use_r("clean_and_tidy_df_func")
@@ -16,7 +21,6 @@ use_r("my_boxplot_and_jitter_func")
 use_r("my_cum_occu_wave_plot_func")
 use_r("pptx_presentation_theme_func")
 use_r("multi_plot_list_func")
-use_r("superplot_func")
 use_r("my_occu_bar_plot_func")
 
 use_r("cell_layer_vio_ggplot_func")
@@ -34,52 +38,3 @@ use_r("plot_arrange_by_condition_func")
 
 # build_readme()
 
-df40_o %>%
-  # str
-  # my_cum_occu_wave_plot_func(20)
-  my_occu_bar_plot_func(10)
-# mutate(new_waves = if_else(Wave_latency <= 10 &
-#                              !is.na(Wave_latency) &
-#                              Wave_latency > 0, T, F)) %>%
-# group_by(.data$Animal, .data$Condition, .data$new_waves) %>%
-# select(.data$Animal, .data$Condition, .data$new_waves) %>%
-# summarise(count = n()) %>%
-# mutate(Percentage = count/sum(count) * 100) %>%
-# filter(.data$new_waves == T) #%>%
-# ggplot2::ggplot(ggplot2::aes(x = .data$Animal,
-#                              y = .data$Percentage,
-#                              fill = .data$new_waves,
-#                              alpha = 0.5)) +
-# ggplot2::geom_bar(stat="identity",
-#                   color = "black",
-#                   ggplot2::aes(fill = .data$Animal),
-#                   size = 1)
-mutate(new_waves = if_else(Wave_latency <= 10 &
-                             !is.na(Wave_latency) &
-                             Wave_latency > 0, T, F)) %>%
-  group_by(Animal, Condition, new_waves) %>%
-  select(Animal, Condition, new_waves) %>%
-  summarise(count = n()) %>%
-  # View
-  mutate(Percentage = count/sum(count) * 100) %>%
-  filter(new_waves == T) %>%
-  # View
-  ggplot(aes(x = Animal,
-             y = Percentage,
-             fill = new_waves,
-             alpha = 0.5))  +
-  geom_bar(stat="identity",
-           color = "black",
-           aes(fill = Animal),
-           size = 1) +
-  ggplot2::facet_grid(. ~ .data$Condition)
-head
-
-"~/Dropbox/r_stat/Figures/Intact_cells/raw_linescan/latency_traces" %>%
-  # paste("WT", "ISO", sep = "/") %>%
-  # list.files(pattern = "*Smth\\.tif", full.names = T) %>%
-  # make_profile_func()
-  make_multi_profile_func() %>%
-  # glimpse()
-  str
-?make_profile_func()
