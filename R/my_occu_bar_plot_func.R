dd36aa189004534a336898061b9be989b83afac5
Ruben Lopez <rjlopez2@gmail.com>
2021-08-11 11:30:19 +0200

    betereasthetics in order of arguments displayed

--- R/my_occu_bar_plot_func.R
+++ R/my_occu_bar_plot_func.R
@@ -4,8 +4,8 @@
 #'
 #' @param my_dataset A dataframe with raw data of waves.
 #' @param reffer_wave_thres A double. Reference value in seconds indicating the threshold for the calculus of cumulative waves occurrence. Default to 10.
-#' @param ... additional parameter to be passed to the aesthetic function \code{pptx_presentation_theme_func()}
 #' @param line_zize Integer. the size of the lines to display. Default to 1.
+#' @param ... additional parameter to be passed to the aesthetic function \code{pptx_presentation_theme_func()}
 #'
 #' @return A ggplot object with barplot of waves occurrence in the different animals groups.
 #' @export
