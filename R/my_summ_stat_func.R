1a2c191df8b8cd9cd83d70c6070af15dc89c029d
Ruben Lopez <rjlopez2@gmail.com>
2021-11-03 08:05:21 +0100

    Add conditional management of dataset less than 2 datapoints for shapiro test. update description

--- R/my_summ_stat_func.R
+++ R/my_summ_stat_func.R
@@ -1,12 +1,13 @@
 utils::globalVariables("where")
 #' Summary statistics of multiples variables
 #'
-#'Compute the summary statistics of the multiples variables from the wave analysis of a previously cleaned dataset. Compute the `mean`, `sd`, `sem`, `median`, `n_Waves`, `n_Cells`, `n_Animals` and `Normality_Shapiro_p.` NOTE: Shapiro-Wilk test will fail if less than 2 samples!.
+#'Compute the summary statistics of numeric variables from a given dataset. Compute the `mean`, `sd`, `sem`, `median`, `n_Waves`, `n_Cells`, `n_Animals` and `Normality_Shapiro_p.` NOTE: Shapiro-Wilk test will fail if less than 2 samples!.
 #'
 #' @param my_dataset A previously cleaned dataframe with the function `clean_and_tidy_df_func()`.
 #' @param my_grouping_vars Character vector. A character vector of groups names assigned to perform the aggregation. Don't change at least you know what you are doing!
 #' @param Na_rm Boolean. Shall Nas be removed for summary functions computation?. Default to TRUE.
 #' @param round_to Integer. A value to indicate number of decimals to used in final output. Default to 2.
+#' @param shapiro Character. asses shapiro wilk test for normality. Requiere dataset length bigger than 2, otherwise will raise a warning.
 #'
 #' @return Return a dataframe with the mean, median, SD, SEM, number of elements analized (cells or wave), number of animals, and asses Normality of the distribution with the Shapiro-Wilk test.
 #' @export
@@ -15,19 +16,50 @@ utils::globalVariables("where")
 my_summ_stat_func <- function(my_dataset,
                               my_grouping_vars = c("Animal", "Treatment", "Condition"),
                               Na_rm = TRUE,
-                              round_to = 2){
+                              round_to = 2,
+                              shapiro = "yes"){
 
-  # create summary df
-  summarized_DF <- my_dataset %>%
+  numeric_cols_names <- my_dataset %>%
+    select(where(is.double)) %>%
+    names
+
+  test_minimal_n <- my_dataset %>%
     group_by(across(any_of(my_grouping_vars))) %>%
     summarise(across(where(is.double),
-                     list(n = ~ length(.x),
-                          mean = ~ mean(.x, na.rm = Na_rm),
-                          sd = ~ stats::sd(.x, na.rm = Na_rm),
-                          sem = ~ sem_func(.x, na.rm = Na_rm),
-                          median = ~ stats::median(.x, na.rm = Na_rm),
-                          NormalityShapirop = ~ stats::shapiro.test(.x)$p.value),
-                     .names = "{.col}_{.fn}"), .groups = "drop")
+                     ~ n() <= 2),
+              .groups = "drop") %>%
+    filter(across(where(is.logical), ~ .x == T))
+
+  if(dim(test_minimal_n)[1] != 0){
+    message("You have at least one variable with less than 2 datapoints.\nShapiro  test for normality requires n >= 2. It will not be assesed!")
+    shapiro = "no"
+
+  }
+
+  # create summary df
+
+  switch(shapiro,
+         yes = summarized_DF <- my_dataset %>%
+           group_by(across(any_of(my_grouping_vars))) %>%
+           summarise(across(where(is.double),
+                            list(n = ~ length(.x),
+                                 mean = ~ mean(.x, na.rm = Na_rm),
+                                 sd = ~ stats::sd(.x, na.rm = Na_rm),
+                                 sem = ~ sem_func(.x, na.rm = Na_rm),
+                                 median = ~ stats::median(.x, na.rm = Na_rm),
+                                 NormalityShapirop = ~ stats::shapiro.test(.x)$p.value),
+                            .names = "{.col}_{.fn}"), .groups = "drop"),
+         no = summarized_DF <- my_dataset %>%
+           group_by(across(any_of(my_grouping_vars))) %>%
+           summarise(across(where(is.double),
+                            list(n = ~ length(.x),
+                                 mean = ~ mean(.x, na.rm = Na_rm),
+                                 sd = ~ stats::sd(.x, na.rm = Na_rm),
+                                 sem = ~ sem_func(.x, na.rm = Na_rm),
+                                 median = ~ stats::median(.x, na.rm = Na_rm)),
+                            # NormalityShapirop = ~ stats::shapiro.test(.x)$p.value),
+                            .names = "{.col}_{.fn}"), .groups = "drop")
+  )
 
   # make it tidy
   rearranged_table <- summarized_DF %>%
@@ -62,7 +94,6 @@ my_summ_stat_func <- function(my_dataset,
       relocate(.data$n_Animals,
                .before = .data$n)
   }
-
 
 
   rm(summarized_DF, animal_no, rearranged_table)
