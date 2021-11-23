e508df067aa2bb7dba3678d55e59c2238ca99c92
Ruben Lopez <rjlopez2@gmail.com>
2021-11-23 14:50:18 +0100

    rewriting for better readibility the load_sr_func function

--- R/load_sr_data_func.R
+++ R/load_sr_data_func.R
@@ -8,16 +8,23 @@
 #' @examples # example is missing
 load_sr_data_func <- function(my_dir){
   # file_list <- list.files(files_path, pattern  = "DataTable.xlsx", full.names = TRUE, recursive = TRUE)
-  file_list <- list_dirs_depth_n_func(my_dir, 1) %>%
-    list.files(., pattern = "DataTable\\.xlsx$", full.names = TRUE)
+  file_list <- my_dir %>%
+    list_dirs_depth_n_func( 1) %>%
+    list.files(., pattern = "DataTable\\.xlsx$", full.names = TRUE) %>%
+    tibble(filename = .) %>%
+    filter(!str_detect(filename, pattern = "~")) %>%
+    pull(filename)
   #print(file_list)
   my_data <- purrr::map_dfr(file_list, function(file){
-    cat(paste("reading file ---->>>>", file, sep = "\n"))
+    # message(paste("reading file ---->>>>", file, sep = "\n"))
 
-    full_data <- readxl::read_xlsx(file, sheet = 4, col_names = TRUE, skip = 2)
+    full_data <- readxl::read_xlsx(file,
+                                   sheet = 4,
+                                   col_names = TRUE,
+                                   skip = 2)
   })
 
-# create TExperiment variable
+# create Experiment variable
   my_data$Experiment <- str_sub(my_data$filename, start = 10, end = 15)
 
 
@@ -26,7 +33,7 @@ load_sr_data_func <- function(my_dir){
 
 # create set to factor other variables
   my_data <- my_data %>%
-    mutate(For_Analysis <- T) %>% #create filtering variable
+    mutate(For_Analysis = T) %>% #create filtering variable
     mutate(across(c(.data$Experiment:.data$Animal_No), factor)) %>%
     mutate(Animal = forcats::fct_relevel(.data$Animal,
                                      c("CPVT-WT", "CPVT-HET"),
@@ -38,6 +45,7 @@ load_sr_data_func <- function(my_dir){
                               c("cAMP", "Fab", "Vehicle"),
                               after = 0)) %>%
     mutate(Date = as.character(.data$Date))
+
 
   remove(file_list)
 
