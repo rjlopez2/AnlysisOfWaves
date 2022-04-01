83b63aa9df4b3395b9a15c8d005ed1f280664b6d
Ruben Lopez <rjlopez2@gmail.com>
2022-04-01 15:22:10 +0200

    fix missing prefix for package purrr in maping function

--- tests/testthat/test-my_summ_stat_func.R
+++ tests/testthat/test-my_summ_stat_func.R
@@ -3,7 +3,7 @@ test_that("summary stats produce output in perme cells df", {
   summary_params <- c("n_Animals", "n", "mean", "sd", "sem", "median", "NormalityShapirop")
   summrized_df <- clean_SR_data %>% my_summ_stat_func()
 
-  map(summary_params, ~ expect_output(str(summrized_df), .x))
+  purrr::map(summary_params, ~ expect_output(str(summrized_df), .x))
 
 
 
@@ -15,7 +15,7 @@ test_that("summary stats produce output in Intact cells df", {
   summary_params <- c("n_Animals", "n", "mean", "sd", "sem", "median", "NormalityShapirop")
   summrized_df <- df_waves_intact_cells %>% my_summ_stat_func()
 
-  map(summary_params, ~ expect_output(str(summrized_df), .x))
+  purrr::map(summary_params, ~ expect_output(str(summrized_df), .x))
 
 
 
