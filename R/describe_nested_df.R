47057a3566d0d7e0c08b2116d70ed04088ed81e0
Ruben Lopez <rjlopez2@gmail.com>
2022-06-09 22:30:20 +0200

    allow for faceting parameter in qq-plot

--- R/describe_nested_df.R
+++ R/describe_nested_df.R
@@ -3,12 +3,18 @@
 #' This function compute descriptive statistics on a nested dataframe and create 3 new columns containing: 1) descriptive statistics using the function `my_summ_stat_func()`, 2) outliers and extreme values and 3) qqplot to visulaize normality.
 #'
 #' @param nested_df A nested dataset. Ideally, previously grouped by the paramters of interes to be analyzed. Eg. wave frequency, wave latency, amplitude, etc.
+#' @param faceted_by_1 A string. Grouping variables for visualization. Default to `Condition`.
+#' @param faceted_by_2 A string. Aditional grouping variables for visualization. Default to `.`.
 #' @param targed_dataset Character. the column name of the dataset to perform the summary. By default uses the colum named `"data"` containing the raw dataset, but if you have transformed data e.ge logarithmic, centered, etc., you can input the name of such column.
+#'
 #' @return A nested dataframe with the additional columns: "Described", "outliers" and "qqplots" for the given input dataframe.
 #' @export
 #'
 #' @examples # the example is missing
-describe_nested_df <- function(nested_df, targed_dataset = "data"){
+describe_nested_df <- function(nested_df,
+                               targed_dataset = "data",
+                               faceted_by_1 = "Condition",
+                               faceted_by_2 = "."){
 
   targed_dataset <- sym(targed_dataset)
    my_parameter_name <- sym(names(nested_df)[1])
@@ -42,7 +48,8 @@ describe_nested_df <- function(nested_df, targed_dataset = "data"){
                                                       qqplotr::stat_qq_line() +
                                                       qqplotr::stat_qq_point() +
                                                       ggplot2::ggtitle(label = paste0("Parameter = ", !!my_parameter_name)) +
-                                                      ggplot2::facet_grid(~Condition, scales = "free_x") +
+                                                      # ggplot2::facet_grid(~Condition, scales = "free_x") +
+                                                      ggplot2::facet_grid(stats::reformulate(faceted_by_1, faceted_by_2), scales = "free_x") +
                                                       ggplot2::labs(x = "Theoretical Quantiles", y = "Sample Quantiles")))) #%>%
     # Check for homogeneity of variance via levene's test and report p value
 
