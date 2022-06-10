e3cc2a3042ba83b8c22bf0cf30d0ca7700087f2d
Ruben Lopez <rjlopez2@gmail.com>
2022-06-10 14:57:54 +0200

    allow options for faceting in the residuals plot function

--- R/plot_residuals_func.R
+++ R/plot_residuals_func.R
@@ -4,13 +4,18 @@
 #'
 #' @param model_nsdf A nested df containing a model object and previously processed with the `extract_model_info()` function.
 #' @param augmented_df The column name containing the df with the fit vs residuals values of the model. By defoult this columns in named `"augment"`.
+#' @param faceted_by_1 A string. Grouping variables for visualization. Default to `Condition + Animal`.
+#' @param faceted_by_2 A string. Aditional grouping variables for visualization. Default to `.`.
 #'
 #' @return A new colum called `res_vs_fit_plot` containing ggplot objets with the residual plots of the model.
 #' @export
 #'
 #' @examples # No run
 #'
-plot_residuals_func <- function(model_nsdf, augmented_df = "augment"){
+plot_residuals_func <- function(model_nsdf,
+                                augmented_df = "augment",
+                                faceted_by_1 = "Condition  + Animal",
+                                faceted_by_2 = "."){
   augmented_df <- sym(augmented_df)
   my_parameter_name <- sym(names(model_nsdf)[1])
   model_name <- sym(names(model_nsdf)[3])
@@ -31,7 +36,8 @@ plot_residuals_func <- function(model_nsdf, augmented_df = "augment"){
                    color = "black",
                    linetype = "dashed",
                    alpha = 0.5) +
-        ggplot2::facet_grid(. ~ Condition  + Animal ) +
+        # ggplot2::facet_grid(. ~ Condition  + Animal ) +
+        ggplot2::facet_grid(stats::reformulate(faceted_by_1, faceted_by_2)) +
         ggplot2::scale_colour_manual(values = c("#666666",
                                                 "#CC0000")) +
         ggplot2::scale_fill_manual(values = c("#666666",
