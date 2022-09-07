859c85c2d1f9368f58aa9f6b3bd40f209fb00ce9
Ruben Lopez <rjlopez2@gmail.com>
2022-09-07 21:28:38 +0200

    replacing index for names of varibales "my_parameter_name" and "model_name" for actual names.

--- R/plot_residuals_func.R
+++ R/plot_residuals_func.R
@@ -3,13 +3,13 @@
 #' This functions take a nested df containing a model information including the column `augment` and return the residuals for a given model. Use this function chained with the `extract_model_info()`.
 #'
 #' @param model_nsdf A nested df containing a model object and previously processed with the `extract_model_info()` function.
-#' @param augmented_df The column name containing the df with the fit vs residuals values of the model. By defoult this columns in named `"augment"`.
+#' @param augmented_df The column name containing the df with the fit vs residuals values of the model. By default this columns in named `"augment"`.
 #' @param faceted_by_1 A string. Grouping variables for visualization. Default to `Condition + Animal`.
-#' @param faceted_by_2 A string. Aditional grouping variables for visualization. Default to `.`.
+#' @param faceted_by_2 A string. Additional grouping variables for visualization. Default to `.`.
 #'
-#' @return A new colum called `res_vs_fit_plot` containing ggplot objets with the residual plots of the model.
+#' @return A new column called `res_vs_fit_plot` containing ggplot objects with the residual plots of the model.
 #' @export
-#'
+#' @note Make sure that the column of with your variable names are named `Parameters` or `variables` otherwise the plot names will crash. Similarly, name your column with the name of the model as `model_name` or could output unexpected results.
 #' @examples # No run
 #'
 plot_residuals_func <- function(model_nsdf,
@@ -17,8 +17,10 @@ plot_residuals_func <- function(model_nsdf,
                                 faceted_by_1 = "Condition  + Animal",
                                 faceted_by_2 = "."){
   augmented_df <- sym(augmented_df)
-  my_parameter_name <- sym(names(model_nsdf)[1])
-  model_name <- sym(names(model_nsdf)[3])
+  # my_parameter_name <- sym(names(model_nsdf)[1])
+  my_parameter_name <- sym(names(model_nsdf)[which(names(model_nsdf) %in% c("variables", "Parameters"))])
+  # model_name <- sym(names(model_nsdf)[3])
+  model_name <- sym(names(model_nsdf)[which(names(model_nsdf) %in% "model_name")])
 
 
 
